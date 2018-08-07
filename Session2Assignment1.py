9b2f3ad9ea69fd382fbd0c6750f9d10991dae851
Deepak Ray <deepthnk2016@gmail.com>
2018-08-07 11:15:03 +0530

    Add files via upload

---
+++ Session2Assignment1.py
@@ -0,0 +1,8 @@
+#-----------------------------------------------------------------
+# Python - Session 2 Assignment 1
+# Descr - Generate a List from comma separated values enterd in console.
+#-----------------------------------------------------------------
+seriesofinput=input("Enter numbers in , separated : ")
+print("Printing the input into list :")
+list=seriesofinput.split(",")
+print(list)
